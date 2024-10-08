b2a817d11e703fc64610c737c6797bfa2dd9dbd3
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-10-09 06:23:34 +0700

    feat: Add Navbar component to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from 'next/font/google';
 import "./globals.css";
+import { Navbar } from "@/components/navbar";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} antialiased`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
