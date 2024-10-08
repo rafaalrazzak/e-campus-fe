c05d4c20fe791dd6bbcf17c87d4534297a0f324c
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-10-09 06:22:31 +0700

    feat: Add formatDate function to format dates using date-fns library

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { format } from "date-fns"
+import { id } from "date-fns/locale"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+
+export function formatDate(date: Date) {
+  return format(date, "iiii, dd MMMM yyyy", {
+    locale: id
+  })
 }
