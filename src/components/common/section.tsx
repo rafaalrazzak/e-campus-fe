d75933f93a542d09fe65abcea881e3ae4842e467
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-10-09 13:52:48 +0700

    feat: Add Section component
    Add a new component called Section that represents a section with a title and children. This component is used to structure the layout of the application and display content in a visually appealing way.

---
+++ src/components/common/section.tsx
@@ -0,0 +1,13 @@
+export type SectionProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+export function Section({ title, children }: SectionProps) {
+  return (
+    <section className="flex flex-col gap-4">
+      <h1 className="text-2xl font-bold text-primary">{title}</h1>
+      {children}
+    </section>
+  );
+}
