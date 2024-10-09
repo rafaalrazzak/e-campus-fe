26f174fba60272e7e7f85696994784582f6c4c07
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-10-09 11:59:27 +0700

    refactor: Update Hero component with AnimatedBackground and styling options

--- src/components/common/hero.tsx
+++ src/components/common/hero.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
 import { cn } from "@/lib/utils";
-import React from "react";
+import { AnimatedBackground } from "./animate-background";
 
 export type HeroProps = {
   onlyImage?: boolean;
@@ -12,25 +11,21 @@ export function Hero({ onlyImage = false, children, className }: HeroProps) {
   return (
     <section
       className={cn(
-        "relative flex min-h-[50vh] flex-col items-center justify-center overflow-hidden rounded-b-3xl p-8 text-center text-white",
+        "relative flex min-h-[50vh] flex-col items-center justify-center overflow-hidden rounded-b-3xl p-8 text-center",
         className,
       )}
     >
-      <Image
-        src="https://assets.siakadcloud.com/uploads/sttnurulfikri/bgaplikasi/1405.jpg"
-        alt="Hero"
-        fill
-        className="-z-10 object-cover object-center brightness-[0.2]"
-      />
-      <div className="container relative mx-auto flex flex-col justify-center gap-8">
+      <AnimatedBackground />
+
+      <div className="container relative z-20 mx-auto flex flex-col justify-center gap-8">
         {!onlyImage && (
           <div className="flex flex-col gap-6 text-center">
-            <h1 className="text-4xl font-bold">
+            <h1 className="text-4xl font-bold text-primary sm:text-5xl md:text-6xl">
               Selamat datang di
               <br />
               E-Campus STT Nurul Fikri
             </h1>
-            <p className="mx-auto max-w-lg">
+            <p className="mx-auto max-w-lg text-lg text-secondary-foreground sm:text-xl">
               Membangun masa depan melalui pendidikan teknologi yang inovatif
               dan berkualitas.
             </p>
