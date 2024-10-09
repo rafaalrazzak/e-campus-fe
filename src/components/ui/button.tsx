2761a5c97df314f737b2704f98c055179cc5b3b2
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-10-09 13:50:29 +0700

    refactor: Update button component styling options
    - Removed unnecessary classes from the button component.
    - Added new options for rounded corners and border styles.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "relative inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-all duration-150 ease-in-out focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 disabled:cursor-not-allowed",
+  "relative inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-all duration-150 ease-in-out focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 disabled:cursor-not-allowed",
   {
     variants: {
       variant: {
@@ -48,10 +48,21 @@ const buttonVariants = cva(
         full: "w-full px-4 py-2",
         fit: "w-fit px-4 py-2",
       },
+      rounded: {
+        default: "rounded-md",
+        full: "rounded-full",
+        none: "rounded-none",
+      },
+      border: {
+        default: "border",
+        none: "border-none",
+      }
     },
     defaultVariants: {
       variant: "primary",
       size: "default",
+      rounded: "default",
+      border: "default",
     },
   },
 );
