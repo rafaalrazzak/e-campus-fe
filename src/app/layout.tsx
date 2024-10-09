57d7f4ff1a05f5928ec924db54d53f1aa564313d
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-10-09 13:52:01 +0700

    refactor: Wrap children in a main container in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         <Navbar />
-        <main className="flex flex-col gap-8 pb-8">{children}</main>
+        <main className="flex flex-col gap-8 pb-8"> {children}</main>
       </body>
     </html>
   );
