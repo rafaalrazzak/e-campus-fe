0568198d9d1c4957df22b0657c8c18be73c9ba84
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-10-09 13:50:17 +0700

    refactor: Update Input component to support left and right icons

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,21 +1,37 @@
 import * as React from "react";
-
 import { cn } from "@/lib/utils";
 
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
+};
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, leftIcon, rightIcon, ...props }, ref) => {
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-10 w-full rounded-md border border-border bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className,
+      <div className="relative w-full">
+        {leftIcon && (
+          <span className="pointer-events-none absolute left-3 top-1/2 -translate-y-1/2 transform">
+            {leftIcon}
+          </span>
         )}
-        ref={ref}
-        {...props}
-      />
+        <input
+          type={type}
+          className={cn(
+            "flex h-10 w-full rounded-md border border-border bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            leftIcon ? "pl-10" : "",
+            rightIcon ? "pr-10" : "",
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+        {rightIcon && (
+          <span className="pointer-events-none absolute right-3 top-1/2 -translate-y-1/2 transform">
+            {rightIcon}
+          </span>
+        )}
+      </div>
     );
   },
 );
