dc291cb1dfb3a2c51aae370292bf020ca9897c5a
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-10-10 08:05:52 +0700

    refactor: Update Section component to include an optional button
    This commit updates the Section component to include an optional button. The button can be passed as a prop and will be rendered alongside the section title. This allows for more flexibility in the usage of the Section component.

--- src/components/common/section.tsx
+++ src/components/common/section.tsx
@@ -1,12 +1,16 @@
 export type SectionProps = {
     title: string;
     children: React.ReactNode;
+    button?: React.ReactNode;
 };
 
-export function Section({ title, children }: SectionProps) {
+export function Section({ title, children, button }: SectionProps) {
     return (
         <section className="flex flex-col gap-4">
-            <h1 className="text-2xl font-bold text-primary">{title}</h1>
+            <div className="flex items-center justify-between gap-2">
+                <h1 className="text-2xl font-bold text-primary">{title}</h1>
+                {button && button}
+            </div>
             {children}
         </section>
     );
