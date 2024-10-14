aecc881a3c71fe3736dcb2e42d34b2bcdea42158
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-10-14 11:07:14 +0700

    Refactor component layouts and add AccordionDesigns to DesignPage

--- src/app/design/page.tsx
+++ src/app/design/page.tsx
@@ -1,8 +1,10 @@
+import { AccordionDesigns } from "@/components/ui/story/accourdion";
 import { BadgeDesigns } from "@/components/ui/story/badge.story";
 import { ButtonDesings } from "@/components/ui/story/button.story";
 
 const DesignPage: React.FC = () => (
     <div className="container mx-auto my-8 flex flex-col gap-8 px-4">
+        <AccordionDesigns />
         <ButtonDesings />
         <BadgeDesigns />
     </div>
