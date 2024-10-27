1febcfae1aa643853437259d2aae82afa7939fc0
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-10-27 13:32:10 +0700

    feat: Add session route to handle authentication
    This commit adds a new file, `route.ts`, which contains the logic for handling authentication sessions. It imports the necessary constants and libraries, and defines a `GET` function that retrieves the session data from the server. If the session cookie is not present or the session is invalid, it returns a 401 Unauthorized response. Otherwise, it returns the session data as a JSON response with a 200 OK status.

---
+++ src/app/api/sessions/route.ts
@@ -0,0 +1,40 @@
+import { Cookies } from "@/constants";
+import { api } from "@/lib/api";
+import { SessionData } from "@/types/api";
+import { cookies } from "next/headers";
+
+export async function GET() {
+    const cookieStore = await cookies();
+
+    const sessionCookie = cookieStore.get(Cookies.SESSION);
+
+    if (!sessionCookie) {
+        return new Response(null, {
+            status: 401,
+            statusText: "Unauthorized",
+        });
+    }
+
+    const session = await api.get<SessionData>("auth/session", {
+        headers: {
+            "Authorization": sessionCookie.value,
+        }
+    })
+
+    if (!session) {
+        return new Response(null, {
+            status: 401,
+            statusText: "Unauthorized",
+        });
+    }
+
+    const response = await session.json();
+
+    return new Response(JSON.stringify(response), {
+        status: 200,
+        statusText: "OK",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+}
