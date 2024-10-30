d4146c279c4159790378d03cb42cec388dacab18
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-10-30 18:24:47 +0700

    refactor: Update SessionData type with additional fields and type changes

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,12 +1,17 @@
 export type SessionData = {
-    address: string;
-    department_id: string;
-    email: string;
-    entry_year: string;
-    id: string;
-    name: string;
+    id: number;
     nim_nip: string;
-    photo_url: string;
+    name: string;
+    email: string;
     role: string;
+    department_code: string;
+    entry_year: number;
     status: string;
+    address: string;
+    created_at: string;
+    updated_at: string;
+    department_name: string;
+    photo_url?: string; 
+    study_plan_status?: string;
+    study_plan_grade?: string;
 };
