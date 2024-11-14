0ecc1fd05dbdcf4675c6b9fe70387002193301ee
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-11-14 19:37:43 +0700

    feat: Enhance date formatting functions and add utility for day names

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,8 +7,17 @@ export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
 
-export function formatDate(date: Date) {
-    return format(date, "iiii, dd MMMM yyyy", {
+export function formatDate(date: Date, detailed = false) {
+    return format(date, detailed ? "iiii, dd MMM yyyy, HH:mm" : "iiii, dd MMMM yyyy", {
         locale: id,
     });
 }
+
+export const formatDay = (date: Date) => format(date, "EEEE", { locale: id });
+
+export const formatTime = (date: Date) => format(date, "HH:mm", { locale: id });
+
+export const getDayName = (day: number) => {
+    const dayNames = ["", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"];
+    return dayNames[day] || "Tidak Diketahui";
+};
