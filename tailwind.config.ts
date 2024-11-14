2a01fd8008364f7676eee4b826c54e759afae3ae
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-11-14 19:38:11 +0700

    feat: Adjust border radius and center container in Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,9 @@ const config: Config = {
     darkMode: ["class"],
     content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
     theme: {
+        container: {
+            center: true,
+        },
         extend: {
             colors: {
                 background: "hsl(var(--background))",
