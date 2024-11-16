76445b2e7e77386b3117bcd2d5919907db44d524
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-11-16 10:56:50 +0700

    fix: Remove unnecessary semicolon from QuizClient component rendering

--- src/app/(user)/dashboard/academic/courses/[slug]/quiz/[id]/page.tsx
+++ src/app/(user)/dashboard/academic/courses/[slug]/quiz/[id]/page.tsx
@@ -4,7 +4,7 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
     const id = (await params).id;
     return (
         <div className="container p-4">
-            <QuizClient />;
+            <QuizClient />
         </div>
     );
 }
