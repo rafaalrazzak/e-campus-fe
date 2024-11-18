abd788b6d3674ee5d49119bca52450eae59e278a
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-11-18 09:56:36 +0700

    Revert "refactor: Replace QuizClient with Quiz component in quiz page"
    This reverts commit 2928366838c407d477637c6df30a097b48a3c026.

--- src/app/(user)/dashboard/academic/courses/[slug]/quiz/[id]/page.tsx
+++ src/app/(user)/dashboard/academic/courses/[slug]/quiz/[id]/page.tsx
@@ -1,10 +1,10 @@
-import { Quiz } from "@/components/common/quiz";
+import { QuizClient } from "./client";
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
     const id = (await params).id;
     return (
         <div className="container p-4">
-            <Quiz />
+            <QuizClient />
         </div>
     );
 }
