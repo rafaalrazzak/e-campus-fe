56cdd3d6f42fc5561cddb9881699cc404c2ee1dd
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-11-24 17:05:00 +0700

    feat: Wrap Quiz component in DashboardLayout to enhance structure and add breadcrumb support

--- src/app/(user)/dashboard/academic/courses/[slug]/quiz/[id]/page.tsx
+++ src/app/(user)/dashboard/academic/courses/[slug]/quiz/[id]/page.tsx
@@ -1,10 +1,11 @@
+import { DashboardLayout } from "@/components/common/dashboard/layout";
 import { Quiz } from "@/components/common/quiz";
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
     const id = (await params).id;
     return (
-        <div className="container p-4">
+        <DashboardLayout title="Kuis" withPadding withBreadcrumb>
             <Quiz />
-        </div>
+        </DashboardLayout>
     );
 }
