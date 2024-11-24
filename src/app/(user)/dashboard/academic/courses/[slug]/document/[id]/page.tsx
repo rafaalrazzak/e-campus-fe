dfee4e37a62d3a4225d2efe67e48a24c590b9bc4
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-11-24 17:05:07 +0700

    feat: Enable breadcrumb support in Document page for improved navigation

--- src/app/(user)/dashboard/academic/courses/[slug]/document/[id]/page.tsx
+++ src/app/(user)/dashboard/academic/courses/[slug]/document/[id]/page.tsx
@@ -4,7 +4,7 @@ import { DocViewer } from "@/components/common/doc-viewer";
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
     const id = (await params).id;
     return (
-        <DashboardLayout title="Dokumen">
+        <DashboardLayout title="Dokumen" withBreadcrumb>
             <DocViewer url="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf" />
         </DashboardLayout>
     );
