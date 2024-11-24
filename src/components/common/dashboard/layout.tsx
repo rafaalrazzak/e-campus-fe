085976aede2ab9b82052cd1126d24436adaf577e
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-11-24 17:54:30 +0700

    feat: Enhance DashboardLayout with ScrollArea and ScrollBar for improved scrolling experience

--- src/components/common/dashboard/layout.tsx
+++ src/components/common/dashboard/layout.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { Separator } from "@/components/ui";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
 import { cn } from "@/lib/utils";
 import { BreadcrumbPath } from "../breadcrumb";
@@ -17,9 +20,9 @@ export function DashboardLayout({
     withBreadcrumb?: boolean;
 }) {
     return (
-        <SidebarInset>
-            <header className={cn("flex h-16 shrink-0 items-center", withBreadcrumb ? "border-b h-24" : "border-b-0")}>
-                <div className="flex flex-col gap-2 px-4">
+        <SidebarInset className="h-[90vh] flex flex-col">
+            <header className={cn("flex shrink-0 border-b", withBreadcrumb ? "h-24" : "h-16")}>
+                <div className="flex flex-col justify-center gap-2 px-4">
                     <div className="flex items-center gap-2">
                         <SidebarTrigger className="-ml-1" />
                         <Separator orientation="vertical" className="mr-2 h-4" />
@@ -29,7 +32,10 @@ export function DashboardLayout({
                 </div>
             </header>
 
-            <div className={cn("flex flex-1 flex-col gap-4", withPadding && "p-4", className)}>{children}</div>
+            <ScrollArea className="flex-1">
+                <ScrollBar />
+                <div className={cn("flex flex-col gap-4", withPadding && "p-4", className)}>{children}</div>
+            </ScrollArea>
         </SidebarInset>
     );
 }
