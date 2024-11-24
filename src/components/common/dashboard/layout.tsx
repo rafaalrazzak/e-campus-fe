f4b0c38e3e0202a30d443e03345ea1b1e15a494f
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-11-24 17:04:08 +0700

    feat: Enhance DashboardLayout to support optional breadcrumb and custom className

--- src/components/common/dashboard/layout.tsx
+++ src/components/common/dashboard/layout.tsx
@@ -1,24 +1,35 @@
 import { Separator } from "@/components/ui";
 import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
 import { cn } from "@/lib/utils";
+import { BreadcrumbPath } from "../breadcrumb";
 
-export function DashboardLayout({ title, children, withPadding }: { title: string; children: React.ReactNode; withPadding?: boolean }) {
+export function DashboardLayout({
+    title,
+    children,
+    className,
+    withPadding = false,
+    withBreadcrumb = false,
+}: {
+    title: string;
+    children: React.ReactNode;
+    className?: string;
+    withPadding?: boolean;
+    withBreadcrumb?: boolean;
+}) {
     return (
         <SidebarInset>
-            <header className="flex h-16 shrink-0 items-center gap-2">
-                <div className="flex items-center gap-2 px-4">
-                    <SidebarTrigger className="-ml-1" />
-                    <Separator orientation="vertical" className="mr-2 h-4" />
-                    <h1 className="text-lg font-semibold line-clamp-1">{title}</h1>
+            <header className={cn("flex h-16 shrink-0 items-center", withBreadcrumb ? "border-b h-24" : "border-b-0")}>
+                <div className="flex flex-col gap-2 px-4">
+                    <div className="flex items-center gap-2">
+                        <SidebarTrigger className="-ml-1" />
+                        <Separator orientation="vertical" className="mr-2 h-4" />
+                        <h1 className="text-lg font-semibold line-clamp-1">{title}</h1>
+                    </div>
+                    {withBreadcrumb && <BreadcrumbPath />}
                 </div>
             </header>
-            <div
-                className={cn("flex flex-1 flex-col gap-4 pb-4", {
-                    "p-4": withPadding,
-                })}
-            >
-                {children}
-            </div>
+
+            <div className={cn("flex flex-1 flex-col gap-4", withPadding && "p-4", className)}>{children}</div>
         </SidebarInset>
     );
 }
