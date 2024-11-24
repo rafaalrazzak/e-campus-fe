36e61062f576366412900981690c0bfb399f52b0
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-11-24 10:20:39 +0700

    feat: Add getFileExtension utility to map MIME types to file extensions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { DOCUMENT_MIME_TYPES } from "@/constants";
 import { type ClassValue, clsx } from "clsx";
 import { format as formatDateFns } from "date-fns";
 import { id } from "date-fns/locale";
@@ -29,4 +30,20 @@ export const getDayName = (date: Date) => {
 export const getTimeRange = (start: Date, duration: number) => {
     const end = new Date(start.getTime() + duration * 60000);
     return `${dateFormat(start, "HH:mm")} - ${dateFormat(end, "HH:mm")}`;
+};
+
+export const getFileExtension = (mimeType: string): string => {
+    const mimeToExtension: Record<string, string> = {
+        [DOCUMENT_MIME_TYPES.PDF]: ".pdf",
+        [DOCUMENT_MIME_TYPES.DOC]: ".doc",
+        [DOCUMENT_MIME_TYPES.DOCX]: ".docx",
+        [DOCUMENT_MIME_TYPES.PPT]: ".ppt",
+        [DOCUMENT_MIME_TYPES.PPTX]: ".pptx",
+        [DOCUMENT_MIME_TYPES.XLS]: ".xls",
+        [DOCUMENT_MIME_TYPES.XLSX]: ".xlsx",
+        [DOCUMENT_MIME_TYPES.JPEG]: ".jpg",
+        [DOCUMENT_MIME_TYPES.PNG]: ".png",
+        [DOCUMENT_MIME_TYPES.SVG]: ".svg",
+    };
+    return mimeToExtension[mimeType] || "";
 };
