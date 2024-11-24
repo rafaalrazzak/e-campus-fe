4d469f9263e1a3b46f77b7554d35218091ca2f9b
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-11-24 12:16:21 +0700

    feat: Add document viewing page with DocViewer component

---
+++ src/app/(user)/dashboard/academic/courses/[slug]/document/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { DashboardLayout } from "@/components/common/dashboard/layout";
+import { DocViewer } from "@/components/common/doc-viewer";
+
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+    const id = (await params).id;
+    return (
+        <DashboardLayout title="Dokumen">
+            <DocViewer url="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf" />
+        </DashboardLayout>
+    );
+}
