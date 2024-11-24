6242ad6a4d629587e3209449e224dde6a790d994
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-11-24 17:05:42 +0700

    fix: Add missing semicolon in DocViewer component for consistency

--- src/components/common/doc-viewer.tsx
+++ src/components/common/doc-viewer.tsx
@@ -3,5 +3,5 @@ interface DocViewerProps {
 }
 
 export function DocViewer({ url }: DocViewerProps) {
-    return <iframe src={`https://docs.google.com/viewer?url=${url}&embedded=true`} width="100%" height="100%" />
+    return <iframe src={`https://docs.google.com/viewer?url=${url}&embedded=true`} width="100%" height="100%" />;
 }
