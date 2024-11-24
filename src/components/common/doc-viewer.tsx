42f87f78d43eb23570bb2784a1addd62228d866d
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-11-24 12:16:16 +0700

    feat: Add DocViewer component for displaying documents in an iframe

---
+++ src/components/common/doc-viewer.tsx
@@ -0,0 +1,7 @@
+interface DocViewerProps {
+    url: string;
+}
+
+export function DocViewer({ url }: DocViewerProps) {
+    return <iframe src={`https://docs.google.com/viewer?url=${url}&embedded=true`} width="100%" height="100%" />
+}
