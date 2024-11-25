1c9949e878d7c14fb1aad36d6c0787f50da16bc8
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-11-25 12:06:11 +0700

    fix: Update Tugas URL in app-sidebar for correct navigation

--- src/components/common/dashboard/app-sidebar.tsx
+++ src/components/common/dashboard/app-sidebar.tsx
@@ -37,7 +37,7 @@ const data = {
                 },
                 {
                     title: "Tugas",
-                    url: "#",
+                    url: URLS.dashboard.accademic.tasks.base,
                 },
             ],
         },
